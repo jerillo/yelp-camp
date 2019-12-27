b607133b5f160a7b3f4664a93602ba05c93a27d5
Jesnine Erillo <jesninenica@yahoo.com>
2019-12-27 15:26:18 -0800

    Added Mongoose

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-let campgrounds = [
-    {name: "Big Bear", image: "https://images.unsplash.com/photo-1487975460695-a2e5c4ea12c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80"},
-    {name: "Lake Arrowhead", image: "https://images.unsplash.com/photo-1484179544078-56b3c0e9ca7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80"},
-    {name: "Joshua Tree", image: "https://images.unsplash.com/photo-1486012345871-f47c8c407079?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"}
-];
+// SCHEMA SETUP
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+const Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, (err, allCampgrounds) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", (req, res) => {
@@ -24,9 +34,15 @@ app.post("/campgrounds", (req, res) => {
     const name = req.body.name;
     const image = req.body.image;
     const newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    // Redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    // Create a new campground and save to DB
+    Campground.create(newCampground, (err, newlyCreated) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // Redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", (req, res) => {
