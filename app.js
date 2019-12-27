6ecece2e54493c7cd5413dad4124367f7f14e85d
Jesnine Erillo <jesninenica@yahoo.com>
2019-12-27 13:12:09 -0800

    Allow adding new campgrounds

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+let campgrounds = [
+    {name: "Big Bear", image: "https://images.unsplash.com/photo-1487975460695-a2e5c4ea12c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80"},
+    {name: "Lake Arrowhead", image: "https://images.unsplash.com/photo-1484179544078-56b3c0e9ca7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80"},
+    {name: "Joshua Tree", image: "https://images.unsplash.com/photo-1486012345871-f47c8c407079?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"}
+];
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-    let campgrounds = [
-        {name: "Big Bear", image: "https://images.unsplash.com/photo-1487975460695-a2e5c4ea12c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1834&q=80"},
-        {name: "Lake Arrowhead", image: "https://images.unsplash.com/photo-1484179544078-56b3c0e9ca7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80"},
-        {name: "Joshua Tree", image: "https://images.unsplash.com/photo-1486012345871-f47c8c407079?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80"}
-    ];
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", (req, res) => {
+    // Get data from form and add to campgrounds array
+    const name = req.body.name;
+    const image = req.body.image;
+    const newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    // Redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new");
 });
 
 app.listen(3000, () => console.log("YelpCamp has started"));
