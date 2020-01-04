0708a5176a32f175190b9a35534e6f47ce0cdf81
Jesnine Erillo <jesninenica@yahoo.com>
2020-01-03 22:44:08 -0800

    Added comment model and seeded database

--- app.js
+++ app.js
@@ -2,32 +2,13 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+const seedDB = require('./seeds')
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-// SCHEMA SETUP
-const campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-const Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-//     {
-//         name: "Granite Hill",
-//         image: "https://images.unsplash.com/photo-1504851149312-7a075b496cc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-//         description: "Person sitting near bonfire surrounded by trees"
-//     }, (err, campground) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log("NEWLY CREATED CAMPGROUND");
-//         }
-//     }
-// );
+seedDB();
 
 app.get("/", (req, res) => {
     res.render("landing");
@@ -71,10 +52,11 @@ app.get("/campgrounds/new", (req, res) => {
 // SHOW - shows more info about one campground
 app.get("/campgrounds/:id", (req, res) => {
     // Find the campground with provided ID
-    Campground.findById(req.params.id, (err, foundCampground) => {
+    Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
         if (err) {
             console.log(err);
         } else {
+            console.log(foundCampground);
             // Render show template with that campground
             res.render("show", {campground: foundCampground});
         }
