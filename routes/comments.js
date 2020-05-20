a4a8071bbe77082cef63b8ec0ab330c9af2f3b17
jerillo <jesnineerillo@berkeley.edu>
2020-05-20 03:42:05 -0700

    edit and destroy routes for comments

--- routes/comments.js
+++ routes/comments.js
@@ -41,6 +41,38 @@ router.post('/', isLoggedIn, (req, res) => {
     });
 });
 
+// Edit 
+router.get('/:comment_id/edit', (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.render('comments/edit', {campground_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+// Update
+router.put('/:comment_id', (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
+// Destroy route
+router.delete('/:comment_id', (req, res) => {
+    Comment.findByIdAndDelete(req.params.comment_id, (err) => {
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    })})
+
 // Middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
