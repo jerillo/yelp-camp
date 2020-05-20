5aec9481dfd94057692b5a6ca684dcb4099c9028
jerillo <jesnineerillo@berkeley.edu>
2020-05-19 21:07:30 -0700

    require login for comments and fixed navbar

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.get('/', (req, res) => {
     res.render('landing');
 });
@@ -83,7 +88,7 @@ app.get('/campgrounds/:id', (req, res) => {
 // ==================================
 // COMMENTS ROUTES
 // ==================================
-app.get('/campgrounds/:id/comments/new', (req, res) => {
+app.get('/campgrounds/:id/comments/new', isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if (err) {
             console.log(err);
@@ -93,7 +98,7 @@ app.get('/campgrounds/:id/comments/new', (req, res) => {
     });
 });
 
-app.post('/campgrounds/:id/comments', (req, res) => {
+app.post('/campgrounds/:id/comments', isLoggedIn, (req, res) => {
     Campground.findById(req.params.id, (err, campground) => {
         if (err) {
             console.log(err);
@@ -154,5 +159,12 @@ app.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/campgrounds');
 });
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 app.listen(3000, () => console.log('YelpCamp has started'));
