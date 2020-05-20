f1e9ff6c78fa8a8b65a29a1ce8f10604d399a998
jerillo <jesnineerillo@berkeley.edu>
2020-05-20 03:49:26 -0700

    comment authorization

--- routes/comments.js
+++ routes/comments.js
@@ -42,7 +42,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 // Edit 
-router.get('/:comment_id/edit', (req, res) => {
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
         if (err) {
             res.redirect('back');
@@ -53,7 +53,7 @@ router.get('/:comment_id/edit', (req, res) => {
 });
 
 // Update
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', checkCommentOwnership, (req, res) => {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
         if (err) {
             res.redirect('back');
@@ -64,7 +64,7 @@ router.put('/:comment_id', (req, res) => {
 });
 
 // Destroy route
-router.delete('/:comment_id', (req, res) => {
+router.delete('/:comment_id', checkCommentOwnership, (req, res) => {
     Comment.findByIdAndDelete(req.params.comment_id, (err) => {
         if (err) {
             res.redirect('back');
@@ -79,6 +79,24 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect('/login');
+}
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if (err) {
+                res.redirect('back ');
+            } else {
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        });
+    } else {
+        res.redirect('back');
+    }
 }
 
 module.exports = router;
